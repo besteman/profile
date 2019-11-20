2a580518e88ddce3d48d668882e1535e6a26e93e
besteman <ellioy37@gmail.com>
2019-11-19 21:26:19 -0500

    Jumbotron

---
+++ components/jumbotron.js
@@ -0,0 +1,12 @@
+import { Jumbotron } from 'reactstrap';
+
+const HomePageJombotron = () => (
+  <Jumbotron>
+    <h1 className="display-3">Hello, World!</h1>
+    <h2 className="lead">My name is Justin Besteman</h2>
+    <hr className="my-2" />
+    <p>A Web Developer and Software Engineer</p>
+  </Jumbotron>
+);
+
+export default HomePageJombotron;
